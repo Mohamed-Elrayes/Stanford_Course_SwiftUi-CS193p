704c88b7b0ab4013bbe7dbc0ff7489533ef87d78
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-03-15 00:52:09 +0200

    first commit

---
+++ Memorize-game/Memorize-game/Memorize_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_gameApp.swift
+//  Memorize-game
+//
+//  Created by Mohamed Salah on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
