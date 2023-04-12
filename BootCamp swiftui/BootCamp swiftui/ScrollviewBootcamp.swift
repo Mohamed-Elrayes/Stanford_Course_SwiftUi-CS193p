0b9b758f503f798de3569c034a744c3513a666cd
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-04-12 13:26:54 +0200

    scrollview

---
+++ BootCamp swiftui/BootCamp swiftui/ScrollviewBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  ScrollviewBootcamp.swift
+//  BootCamp swiftui
+//
+//  Created by Mohamed Salah on 12/04/2023.
+//
+
+import SwiftUI
+
+struct ScrollviewBootcamp: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ..< 10) { _ in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack {
+                            ForEach(0 ..< 20) { _ in
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(.white)
+                                    .frame(width: 200, height: 150)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollviewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollviewBootcamp()
+    }
+}
