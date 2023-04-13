f930b9ca5296dee9e6f6285b4e57e7058a6aea8c
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-04-13 18:13:03 +0200

    state

---
+++ BootCamp swiftui/BootCamp swiftui/StateBootcamp.swift
@@ -0,0 +1,50 @@
+//
+//  StateBootcamp.swift
+//  BootCamp swiftui
+//
+//  Created by Mohamed Salah on 13/04/2023.
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    @State var background = Color.blue
+    @State var count: Int = 0
+    @State var myTitle: String = "My Title"
+    
+    var body: some View {
+        ZStack {
+            background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text(myTitle)
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.title)
+                    .underline()
+                
+                HStack(spacing: 20) {
+                    Button("Button 1") {
+                        background = .red
+                        myTitle = "Button 1 was preesed"
+                        count += 1
+                        
+                    }.foregroundColor(.white)
+                    
+                    Button("Button 2") {
+                        background = .green
+                        myTitle = "Button 2 was preesed"
+                        count -= 1
+                    }.foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+struct StateBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StateBootcamp()
+    }
+}
