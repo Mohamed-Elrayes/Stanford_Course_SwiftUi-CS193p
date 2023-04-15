8fadd41ab46aa672f1ed4ff37cf73e0494eff376
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-04-16 00:48:57 +0200

    animation

---
+++ BootCamp swiftui/BootCamp swiftui/AnimationBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  AnimationBootcamp.swift
+//  BootCamp swiftui
+//
+//  Created by Mohamed Salah on 14/04/2023.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    @State var isAnimated: Bool = false
+
+    var body: some View {
+        VStack {
+            Button("Button") {
+                isAnimated.toggle()
+//                withAnimation(.default) {
+//                    isAnimated.toggle()
+//                }
+            }
+            RoundedRectangle(
+                cornerRadius: isAnimated ? 25 : 20
+            )
+                .fill(
+                    isAnimated ? Color.red : Color.blue
+                )
+                .frame(
+                    width:
+                        isAnimated ? 50 : 100,
+                    height:
+                        isAnimated ? 50 : 100
+                )
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0 ))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(.default)
+        }
+    }
+}
+
+struct AnimationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationBootcamp()
+    }
+}
