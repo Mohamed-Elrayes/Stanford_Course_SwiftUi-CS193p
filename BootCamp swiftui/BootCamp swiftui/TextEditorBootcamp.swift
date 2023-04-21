068c8d49e832179ae038757c0b0aefa7015f8056
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-04-21 17:40:32 +0200

    Text Editor

---
+++ BootCamp swiftui/BootCamp swiftui/TextEditorBootcamp.swift
@@ -0,0 +1,41 @@
+//
+//  TextEditorBootcamp.swift
+//  BootCamp swiftui
+//
+//  Created by Mohamed Salah on 21/04/2023.
+//
+
+import SwiftUI
+
+struct TextEditorBootcamp: View {
+    @State var textEditorText: String = "This is The Start "
+    @State var saveText: String = ""
+    var body: some View {
+        NavigationView {
+            VStack{
+                TextEditor(text:$textEditorText )
+                
+                    .frame(height: 250)
+                    .colorMultiply(.gray.opacity(0.3))
+                    .cornerRadius(20).padding()
+                    
+                Button("save"){
+                    saveText = textEditorText
+                    
+                }
+                Text(saveText).padding()
+                Spacer()
+                
+            }
+           
+        }
+        .padding()
+        .background(.red)
+    }
+}
+
+struct TextEditorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorBootcamp()
+    }
+}
